a4592b456c7edf64b75cad210de6627d171c23bf
Abdelrahman Hussein <ahussein@codescalers.com>
2017-06-18 15:11:42 +0200

    remove sudo

--- .travis_ci/prepare.sh
+++ .travis_ci/prepare.sh
@@ -16,4 +16,4 @@ source ~/.jsenv.sh
 js9_build -l
 
 # install capnp tools
-ssh -A root@localhost -p 2222 "cd /tmp/;curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz;tar zxf capnproto-c++-0.6.1.tar.gz;cd capnproto-c++-0.6.1;./configure;sudo make install"
+ssh -A root@localhost -p 2222 "cd /tmp/;curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz;tar zxf capnproto-c++-0.6.1.tar.gz;cd capnproto-c++-0.6.1;./configure;make install"
