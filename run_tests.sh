ff46e96cee7a5ca228cd6875eec54f0d0a006c7a
Abdelrahman Hussein <ahussein@codescalers.com>
2017-06-19 17:32:59 +0200

    add logging

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "* Including js environment variables"
 export SSHKEYNAME=main
 source ~/.jsenv.sh
+echo "* Start container"
 js9_start
 ssh -A -i ~/.ssh/main root@localhost -p 2222 'cd /root/gig/code/github/jumpscale/ays9; /bin/bash test.sh'
