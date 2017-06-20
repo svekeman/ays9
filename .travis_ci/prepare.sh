82b771ded59e0edf3bfe67f000a7a90b386d3123
Abdelrahman Hussein <ahussein@codescalers.com>
2017-06-20 12:20:04 +0200

    install capnp tools in the same container

--- .travis_ci/prepare.sh
+++ .travis_ci/prepare.sh
@@ -14,7 +14,3 @@ curl https://raw.githubusercontent.com/Jumpscale/developer/$GIGDEVELOPERBRANCH/j
 # build image
 source ~/.jsenv.sh
 js9_build -l
-
-# install capnp tools
-js9_start
-ssh -A root@localhost -p 2222 "cd /tmp/;curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz;tar zxf capnproto-c++-0.6.1.tar.gz;cd capnproto-c++-0.6.1;./configure;make install"
--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,6 @@ export SSHKEYNAME=main
 source ~/.jsenv.sh
 echo "* Start container"
 js9_start
+# install capnp tools
+ssh -A -i ~/.ssh/main root@localhost -p 2222 "cd /tmp/;curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz;tar zxf capnproto-c++-0.6.1.tar.gz;cd capnproto-c++-0.6.1;./configure;make install"
 ssh -A -i ~/.ssh/main root@localhost -p 2222 'cd /root/gig/code/github/jumpscale/ays9; /bin/bash test.sh'
