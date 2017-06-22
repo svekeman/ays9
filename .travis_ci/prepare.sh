87840e42a280370b66ed97334876c842bdb97b82
Abdelrahman Hussein <ahussein@codescalers.com>
2017-06-22 16:40:22 +0200

    comment out GIGBRANCH export and add failure line

--- .travis_ci/prepare.sh
+++ .travis_ci/prepare.sh
@@ -6,7 +6,7 @@ export SSHKEYNAME=main
 
 export GIGSAFE=1
 # export GIGBRANCH=$(git symbolic-ref --short HEAD)
-export GIGBRANCH=master
+# export GIGBRANCH=master
 export GIGDEVELOPERBRANCH=master
 
 curl https://raw.githubusercontent.com/Jumpscale/developer/$GIGDEVELOPERBRANCH/jsinit.sh?$RANDOM > /tmp/jsinit.sh; bash /tmp/jsinit.sh
