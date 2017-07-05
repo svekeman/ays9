8e414cc083917145a231c21ac6106b2b70514e13
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2017-07-06 00:55:44 +0200

    execute bp and create runs

--- testrunner.py
+++ testrunner.py
@@ -56,19 +56,76 @@ def copy_blueprints(test_bp_path, repo_info):
     j.sal.fs.copyDirTree(test_bp_path, dest)
 
 
+def execute_blueprint(cli, blueprint, repo_info):
+    """
+    Execute a blueprint
+    """
+    errors = []
+    j.logger.logging.info('Executing blueprint [%s]' % blueprint)
+    curdir = j.sal.fs.getcwd()
+    j.sals.fs.changeDir(repo_info[path])
+    cmd = 'ays blueprint -f %s' % blueprint
+    try:
+        j.tools.prefab.get().core.run(cmd)
+        # res, ok = check_status_code(cli.executeBlueprint(data={}, blueprint=blueprint, repository=repo_info['name']))
+        # if not ok:
+        #     j.logger.logging.error('Failed to execute blueprint [%s]. Error: %s' % (blueprint, e))
+        # else:
+        #     j.logger.logging.debug('Executed blueprint [%s]' % blueprint)
+    except Exception as e:
+        errors.append('Failed to execute blueprint [%s]. Error: %s' % (blueprint, e))
+    finally:
+        j.sals.fs.changeDir(curdir)
+    return errors
+
+def create_run(cli, repo_info):
+    """
+    Create a run and executing it
+    """
+    errors = []
+    j.logger.logging.info('Creating a new run')
+    curdir = j.sal.fs.getcwd()
+    j.sals.fs.changeDir(repo_info[path])
+    cmd = 'ays run create -y --force -f'
+    try:
+        j.tools.prefab.get().core.run(cmd)
+    except Exception as e:
+        errors.append('Failed to create run. Error: %s' % (e))
+
+    return errors
+
+def execute_blueprints(cli, repo_info):
+    """
+    Executes all the blueprints in the repo
+    For each blueprint:
+        - execute the blueprint
+        - create a run
+        - execute the run
+        - wait for the run to finish
+        - destory the repo
+    """
+    bps_path = j.sal.fs.joinPaths(repo_info['path'], 'blueprints')
+    blueprints = map(j.sal.fs.getBaseName, j.sal.fs.listFilesInDir(path=bps_path))
+    for blueprint in blueprints:
+        errors.extend(execute_blueprint(cli, blueprint, repo_info))
+        errors.extend(create_run(cli, repo_info))
+        cli.destroyRepository(data={}, repository=repo_info['name'])
+
 
 def main():
     cli = j.clients.atyourservice.get().api.ays
     repo_info = ensure_test_repo(cli, AYS_TESTRUNNER_REPO_NAME)
+    errors = []
 
     if repo_info:
         try:
             copy_blueprints(AYS_CORE_BP_TESTS_PATH, repo_info)
+            execute_blueprints(cli, repo_info)
         finally:
             # clean the created repo
             j.logger.logging.info('Cleaning up ceated repository')
-            # cli.destroyRepository(data={}, repository=repo_info['name'])
-            # cli.deleteRepository(repository=repo_info['name'])
+            cli.destroyRepository(data={}, repository=repo_info['name'])
+            cli.deleteRepository(repository=repo_info['name'])
 
 if __name__ == '__main__':
     main()
