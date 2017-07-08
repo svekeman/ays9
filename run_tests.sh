37dfc2a23cd60b0ff64b047e76c5a88a2d6f162e
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2017-07-09 01:29:22 +0200

    remove debug msg

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@ echo "* Including js environment variables"
 export SSHKEYNAME=main
 source ~/.jsenv.sh
 echo "* Start container"
-echo "GIGBRANCH is ${GIGBRANCH}"
+
 js9_start
 # install capnp tools
 ssh -A -i ~/.ssh/main root@localhost -p 2222 "cd /tmp/;curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz;tar zxf capnproto-c++-0.6.1.tar.gz;cd capnproto-c++-0.6.1;./configure;make install"
