068488b4047ab65deaee353fc1cb2d2383d88a3a
Ahmed T. Youssef <xmonader@gmail.com>
2017-08-10 13:39:04 +0200

    update cons actions

--- tests/sample_repo_basicdelete/actorTemplates/cons/actions.py
+++ tests/sample_repo_basicdelete/actorTemplates/cons/actions.py
@@ -1,30 +1,3 @@
-def init(job):
-    service = job.service
-    aysrepo = service.aysrepo
-    sshactor = aysrepo.actorGet("sshkey")
-    k1 = sshactor.serviceCreate("k1")
-    k2 = sshactor.serviceCreate("k2")
-    k3 = sshactor.serviceCreate("k3")
-
-    service.consume(k1)
-    service.consume(k2)
-    service.consume(k3)
-
-
-def install(job):
-    service = job.service
-    aysrepo = service.aysrepo
-    k1 = aysrepo.serviceGet("sshkey", "k1")
-    k2 = aysrepo.serviceGet("sshkey", "k2")
-    k3 = aysrepo.serviceGet("sshkey", "k3")
-    k3.consume(k2)
-
-
-    k1.save()
-    k3.save()
-    k2.save()
-
-
 
 def stop(job):
     print("Stopping cons cons1")
