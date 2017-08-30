14d683e1a5e6a494306405b3a38e344bc00a43b2
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2017-08-30 02:13:39 +0200

    using docker installer

--- .travis_ci/prepare.sh
+++ .travis_ci/prepare.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-# generate ssh keys
-ssh-keygen -t rsa -N "" -f ~/.ssh/main
-export SSHKEYNAME=main
-
-export GIGSAFE=1
-export GIGDEVELOPERBRANCH=master
-
-curl https://raw.githubusercontent.com/Jumpscale/developer/$GIGDEVELOPERBRANCH/jsinit.sh?$RANDOM > /tmp/jsinit.sh; bash /tmp/jsinit.sh
-
-# build image
-source ~/.jsenv.sh
-js9_build -l
-
+# # generate ssh keys
+# ssh-keygen -t rsa -N "" -f ~/.ssh/main
+# export SSHKEYNAME=main
+#
+# export GIGSAFE=1
+# export GIGDEVELOPERBRANCH=master
+#
+# curl https://raw.githubusercontent.com/Jumpscale/developer/$GIGDEVELOPERBRANCH/jsinit.sh?$RANDOM > /tmp/jsinit.sh; bash /tmp/jsinit.sh
+#
+# # build image
+# source ~/.jsenv.sh
+# js9_build -l
+#
 
 # docker using bash installers
+sudo su -
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
-export ZUTILSBRANCH=master
+export ZUTILSBRANCH=fixes
 export ZBRANCH=9.1.1_remove_gigdir
 curl https://raw.githubusercontent.com/Jumpscale/bash/$ZUTILSBRANCH/install.sh?$RANDOM > /tmp/install.sh;sudo -E bash /tmp/install.sh
+source /opt/code/github/jumpscale/bash/zlibs.sh
 ZCodeGetJS
 ZDockerInstallLocal
 eval $(ssh-agent)
 ssh-add
 ZInstaller_js9_full
+docker stop build
 ZInstaller_ays9
