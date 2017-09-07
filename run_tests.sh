36926b76beb6527eaf9b91285a22db1262236b86
Abdelrahman Hussein <husseina@greenitglobe.com>
2017-09-07 18:40:28 +0200

    update build/test scripts

--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-eval $(ssh-agent)
-ssh-add
 
-# Start ays9 container
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZDockerActive -b jumpscale/ays9 -i ays9"
+export SSHKEYNAME=id_rsa
 
-# Install capnp tools
-sudo -HE bash -c "ssh -tA  root@localhost -p 2222 \"cd /tmp/;curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz;tar zxf capnproto-c++-0.6.1.tar.gz;cd capnproto-c++-0.6.1;./configure;make install\""
+if [ $TRAVIS_EVENT_TYPE == "cron" ]; then
+    # Start ays9 container
+    sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerActive -b jumpscale/ays9 -i ays9"    
+    # Install capnp tools
+    sudo -HE bash -c "ssh -tA  root@localhost -p 2222 \"cd /tmp/;curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz;tar zxf capnproto-c++-0.6.1.tar.gz;cd capnproto-c++-0.6.1;./configure;make install\""
+    # Install RQ
+    sudo -HE bash -c "ssh -tA root@localhost -p 2222 \"pip install rq\""
+else
+    # Start ays9 container
+    sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerActive -b jumpscale/ays9nightly -i ays9"
 
-# Install RQ
-sudo -HE bash -c "ssh -tA root@localhost -p 2222 \"pip install rq\""
+fi
 
 # Run core tests
 sudo -HE bash -c "ssh -tA  root@localhost -p 2222 \"cd /opt/code/github/jumpscale/ays9; /bin/bash test.sh\""
+
+if [ $TRAVIS_EVENT_TYPE == "cron" ]; then
+    # Run non-core tests
+    echo "Running non-core tests"
+fi
