432e13ecc885d85546a4d0f93ee3bebf509ee362
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2017-09-08 11:46:26 +0200

    update run tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 export SSHKEYNAME=id_rsa
 
@@ -12,7 +13,10 @@ if [ $TRAVIS_EVENT_TYPE == "cron" ]; then
 else
     # Start ays9 container
     sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerActive -b jumpscale/ays9nightly -i ays9"
-
+    # Install capnp tools 
+    sudo -HE bash -c "ssh -tA  root@localhost -p 2222 \"cd /tmp/;curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz;tar zxf capnproto-c++-0.6.1.tar.gz;cd capnproto-c++-0.6.1;./configure;make install\""
+    # Install RQ
+    sudo -HE bash -c "ssh -tA root@localhost -p 2222 \"pip install rq\""
 fi
 
 # Run core tests
@@ -22,3 +26,5 @@ if [ $TRAVIS_EVENT_TYPE == "cron" ]; then
     # Run non-core tests
     echo "Running non-core tests"
 fi
+
+sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerCommit -b jumpscale/ays9"
