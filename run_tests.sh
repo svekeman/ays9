e7198e0bb9649154b86cf18b6fa7a936e95f1dd8
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2017-09-08 17:13:25 +0200

    updates

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+set -x
 
 export SSHKEYNAME=id_rsa
 
-if [ $TRAVIS_EVENT_TYPE == "cron" ]; then
+if [ -n $TRAVIS_EVENT_TYPE ] || [ $TRAVIS_EVENT_TYPE == "cron" ]; then
     # Start ays9 container
     sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerActive -b jumpscale/ays9 -i ays9"    
     # Install capnp tools
@@ -13,18 +14,18 @@ if [ $TRAVIS_EVENT_TYPE == "cron" ]; then
 else
     # Start ays9 container
     sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerActive -b jumpscale/ays9nightly -i ays9"
-    # Install capnp tools 
-    sudo -HE bash -c "ssh -tA  root@localhost -p 2222 \"cd /tmp/;curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz;tar zxf capnproto-c++-0.6.1.tar.gz;cd capnproto-c++-0.6.1;./configure;make install\""
-    # Install RQ
-    sudo -HE bash -c "ssh -tA root@localhost -p 2222 \"pip install rq\""
 fi
 
 # Run core tests
 sudo -HE bash -c "ssh -tA  root@localhost -p 2222 \"cd /opt/code/github/jumpscale/ays9; /bin/bash test.sh\""
 
-if [ $TRAVIS_EVENT_TYPE == "cron" ]; then
+if [ -n $TRAVIS_EVENT_TYPE ] || [ $TRAVIS_EVENT_TYPE == "cron" ]; then
     # Run non-core tests
     echo "Running non-core tests"
 fi
 
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerCommit -b jumpscale/ays9"
+if [ -n $TRAVIS_EVENT_TYPE ] || [ $TRAVIS_EVENT_TYPE == "cron" ]; then
+    sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerCommit -b jumpscale/ays9 -i ays9"
+else
+    sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerCommit -b jumpscale/ays9nightly -i ays9"
+fi
--- upload_image.sh
+++ upload_image.sh
@@ -2,7 +2,10 @@
 
 # uploads the successfully built js9 ays image to dockr hub for development re-use
 
-image_id=$(sudo docker images -q jumpscale/ays9)
-sudo docker tag $image_id jumpscale/ays9nightly:latest
+if [ -n $TRAVIS_EVENT_TYPE ] || [ $TRAVIS_EVENT_TYPE == "cron" ]; then
+    image_id=$(sudo docker images -q jumpscale/ays9)
+    sudo docker tag $image_id jumpscale/ays9nightly:latest
+fi
+
 sudo docker login -u ${DOCKERHUB_LOGIN} -p ${DOCKERHUB_PASS}
 sudo docker push jumpscale/ays9nightly
