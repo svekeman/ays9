37b2ab1f15fe8026cf79aa0b38506534f0dc43e9
Yves Kerwyn <yves.kerwyn@greenitglobe.com>
2017-09-26 16:54:27 +0200

    update the ays client factory with get2()

--- JumpScale9AYS/clients/Factory.py
+++ JumpScale9AYS/clients/Factory.py
@@ -1,5 +1,5 @@
 from JumpScale9AYS.clients.sync_client import Client
-
+from JumpScale9AYS.clients.yves_client import Client as Client2
 
 class Factory:
 
@@ -8,3 +8,6 @@ class Factory:
 
     def get(self, base_uri="http://localhost:5000"):
         return Client(base_uri=base_uri)
+
+    def get2(self, base_uri="http://localhost:5000"):
+        return Client2(base_uri=base_uri)
