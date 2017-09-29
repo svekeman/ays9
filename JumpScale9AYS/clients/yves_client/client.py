5f4e0d673fc6872e5f1423ff7dca2e79c4237ab0
Yves Kerwyn <yves.kerwyn@greenitglobe.com>
2017-09-29 14:31:21 +0200

    remove old yves_client directory

--- JumpScale9AYS/clients/yves_client/client.py
+++
@@ -1,9 +0,0 @@
-from js9 import j
-from .Repository import Repositories
-
-#todo: support JWT
-
-class Client:
-    def __init__(self, base_uri="https://localhost:5000"):
-        self._ayscl = j.clients.atyourservice.get(base_uri)
-        self.repositories = Repositories(self)
